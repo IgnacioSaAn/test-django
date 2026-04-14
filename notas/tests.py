f056f0bf3afcc4c42cf3758fe5f57c045f7e02d7
IgnacioSaAn <ignaciosanchezagnello@gmail.com>
2026-04-14 15:31:32 -0300

    Agrego tests de ejemplo para form y creation de user and note

--- notas/tests.py
+++ notas/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Nota
+from .forms import NotaForm
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class NotaTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.nota = Nota.objects.create(titulo='Test Note', contenido='test note ejemplo', usuario=self.user)
+
+    def test_nota_creation(self):
+        self.assertEqual(self.nota.titulo, 'Test Note')
+        self.assertEqual(self.nota.contenido, 'test note ejemplo')
+        self.assertEqual(self.nota.usuario, self.user)
+
+class NotaFormTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.nota = Nota.objects.create(titulo='Test Note', contenido='test note ejemplo', usuario=self.user)
+        self.form_data = {
+            'titulo': 'Test Note',
+            'contenido': 'test note ejemplo',
+        }
+        self.form = NotaForm(data=self.form_data)
+
+    def test_nota_form_valid(self):
+        self.assertTrue(self.form.is_valid())
+
+
+
+#
